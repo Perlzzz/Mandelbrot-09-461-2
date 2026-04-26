63543aad4d5c02655851118eb3934b7df63bcea7
Perlzzz <kirillpontavin@gmail.com>
2026-04-27 02:56:55 +0300

    реализация открытия окна Жюлиа по правому клику мыши, но из-за некорректных координат расчет улетает в бесконечность, и все точки окрашиваются в один цвет

--- src/ru/gr0946x/ui/PaintPanel.java
+++ src/ru/gr0946x/ui/PaintPanel.java
@@ -27,9 +27,23 @@ public class PaintPanel extends JPanel {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                // тут логика
+                // Проверка правой кнопки мыши согласно пункту 1 и 8
+                if (SwingUtilities.isRightMouseButton(e)) {
+                    double mouseX = e.getX();
+                    double mouseY = e.getY();
+
+                    // Преобразование экранных координат в комплексные для константы C
+                    double cRe = (mouseX - getWidth() / 2.0) / 100.0;
+                    double cIm = (mouseY - getHeight() / 2.0) / 100.0;
+
+                    openJuliaSetWindow(cRe, cIm);
+                }
             }
         });
+    }
+    private void openJuliaSetWindow(double cRe, double cIm) {
+        JuliaSetWindow juliaWindow = new JuliaSetWindow(cRe, cIm);
+        juliaWindow.setVisible(true);
     }
     @Override
     public void paint(Graphics g){
