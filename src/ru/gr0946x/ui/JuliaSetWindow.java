a766e8376b7b3d1d2dc52da37d5412f09504a9f9
Perlzzz <kirillpontavin@gmail.com>
2026-04-26 03:33:30 +0300

    создан класс JuliaSetWindow и внутренний класс панели JuliaSetPanel

--- src/ru/gr0946x/ui/JuliaSetWindow.java
+++ src/ru/gr0946x/ui/JuliaSetWindow.java
@@ -7,4 +7,10 @@ import java.awt.image.BufferedImage;
 
 public class JuliaSetWindow extends JFrame {
     private double cRe, cIm;  // комплексная константа для множества Жюлиа
+
+    // внутренний класс панели для рисования множества Жюлиа
+    class JuliaSetPanel extends JPanel {
+        private double cRe, cIm;
+        private BufferedImage image;
+    }
 }
