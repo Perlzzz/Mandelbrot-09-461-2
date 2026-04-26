3f5ed2fd6bb3ee3ab12a3dfc5f4d77b7753b4802
Perlzzz <kirillpontavin@gmail.com>
2026-04-26 03:26:12 +0300

    класс JuliaSetWindow с переменными для комплексной константы

---
+++ src/ru/gr0946x/ui/JuliaSetWindow.java
@@ -0,0 +1,10 @@
+package ru.gr0946x.ui;
+
+// создаем класс окна для множества Жюлиа
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class JuliaSetWindow extends JFrame {
+    private double cRe, cIm;  // комплексная константа для множества Жюлиа
+}
