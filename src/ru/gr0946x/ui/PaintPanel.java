88de2765af555a542743781a3637a27f8758f0f5
f4enow <f4enow@yandex.ru>
2026-05-02 21:10:18 +0300

    Реализован сдвиг изображения при помощи правой кнопки мыши

--- src/ru/gr0946x/ui/PaintPanel.java
+++ src/ru/gr0946x/ui/PaintPanel.java
@@ -13,6 +13,8 @@ import java.awt.event.MouseEvent;
 public class PaintPanel extends JPanel {
 
     protected Painter painter;
+    private Point lastPoint;
+
     public PaintPanel(Painter painter){
         this.painter = painter;
         addComponentListener(new ComponentAdapter() {
@@ -25,9 +27,13 @@ public class PaintPanel extends JPanel {
             }
         });
         // слушатель событий мыши
-        addMouseListener(new MouseAdapter() {
+        MouseAdapter ma = new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e)) {
+                    lastPoint = e.getPoint();
+                }
+
                 // Открытие окна множества Жюлиа по двойному клику левой кнопкой мыши
                 if (SwingUtilities.isLeftMouseButton(e) && e.getClickCount() == 2) {
                     if (painter instanceof FractalPainter fp) {
@@ -38,7 +44,35 @@ public class PaintPanel extends JPanel {
                     }
                 }
             }
-        });
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e)) {
+                    lastPoint = null;
+                }
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e) && lastPoint != null) {
+                    if (painter instanceof FractalPainter fp) {
+                        var conv = fp.getConverter();
+
+                        double dx = conv.xScr2Crt(lastPoint.x) - conv.xScr2Crt(e.getX());
+                        double dy = conv.yScr2Crt(lastPoint.y) - conv.yScr2Crt(e.getY());
+
+                        conv.setXShape(conv.getXMin() + dx, conv.getXMax() + dx);
+                        conv.setYShape(conv.getYMin() + dy, conv.getYMax() + dy);
+
+                        lastPoint = e.getPoint();
+                        repaint();
+                    }
+                }
+            }
+        };
+
+        addMouseListener(ma);
+        addMouseMotionListener(ma);
     }
     private void openJuliaSetWindow(double cRe, double cIm) {
         JuliaSetWindow juliaWindow = new JuliaSetWindow(cRe, cIm);
